e351fd46fc32c36abcba503c2a583bc9a72e2a69
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-10-22 00:46:57 +0200

    modifiy query

--- models/Employee.php
+++ models/Employee.php
@@ -87,7 +87,7 @@
                         // Create query
                         $query = 'SELECT id, Job_title , Salary, Location, YearsExperience
                                                   FROM ' . $this->table . ' 
-                                                 order by YearsExperience desc
+                                                 order by ? desc
                                                   ';    
                 
                         // Prepare statement
