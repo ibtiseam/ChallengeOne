ba5f86fad52eafef90608e6ef5699295f0a3f948
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-10-22 09:51:10 +0200

    modifiy query

--- models/Employee.php
+++ models/Employee.php
@@ -148,7 +148,7 @@
                 
                       
 
-                        $query =  'SELECT '.$fields.' FROM ' . $this->table  ;
+                        $query =  'SELECT' .$fields.' FROM ' . $this->table .'WHERE job_title='.$fields ;
                 
                         $stmt = $this->conn->prepare($query);
                         $stmt->execute();
