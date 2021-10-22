fc84d380526aa0fb00727a132ee9a132c7ee2a62
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-10-22 09:30:42 +0200

    modifiy query

--- models/Employee.php
+++ models/Employee.php
@@ -125,7 +125,7 @@
                      public function read_single_employee() {
       
                       
-                        $selected_fields = array_map('trim', explode(',','Job_title','Salary'));
+                        $selected_fields = array_map('trim', explode(',','Job_title','Location'));
                         $employee_fields = array_map('trim', explode(',', $_GET['fields']));
                         print( $employee_fields);
                 
@@ -135,7 +135,7 @@
                             if (!empty($value)) {
                                    
                                     if (in_array($value, $selected_fields) ) {
-                                        $fields = $value ;
+                                        $fields = $value . ",";
                                     
                                     }
                             }
